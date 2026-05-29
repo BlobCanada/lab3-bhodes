d3f32780e0a8bfe446ef90e5ed504b67693f593c
Ben Hodes <bhodes@myseneca.ca>
2026-05-29 13:36:15 -0400

    Added lab3d

---
+++ lab3d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Author ID: bhodes
+
+import os
+
+def free_space():
+    stream = os.popen("df -h | grep '/$' | awk '{print $4}'")
+    output = stream.read()
+
+    return output.strip()
+
+if __name__ == '__main__':
+    print(free_space)
